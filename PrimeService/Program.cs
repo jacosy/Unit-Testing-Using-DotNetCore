aac1abb842a42ba55834dd0dacf73baabbb3cf23
Longoria You <longoria.you@gmail.com>
2017-05-09 16:23:27 -0600

    First Unit Test in .NetCore Project by MSTest

--- PrimeService/Program.cs
+++ PrimeService/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace PrimeService
+namespace Prime.Service
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var service = new PrimeService();
+            service.IsPrime(1);
         }
     }
 }
