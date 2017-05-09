6b96eb3e2b3328a09672a419650c4a21f4a70e7a
Longoria You <longoria.you@gmail.com>
2017-05-09 14:55:10 -0600

    first commit

---
+++ PrimeService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
